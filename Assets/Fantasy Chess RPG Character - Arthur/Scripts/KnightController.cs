fd6a85ccb2e656e06eefdc9309b1a5e0b50f33b2
Shiran <shiranavr226@gmail.com>
2020-04-12 14:58:59 +0300

    new repository and new name for the game

---
+++ Assets/Fantasy Chess RPG Character - Arthur/Scripts/KnightController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+    float speed = 4;
+    float rotSpeed = 80;
+    float rot = 0f;
+    float gravity = 8;
+
+    Vector3 moveDir = Vector3.zero;
+
+
+    CharacterController controller;
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                anim.SetBool("walk", true);
+                moveDir = new Vector3(0, 0, 1);
+                moveDir *= speed;
+                moveDir = transform.TransformDirection(moveDir);
+            }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                anim.SetBool("walk", false);
+                moveDir = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("walk", false);
+                moveDir = new Vector3(0, 1, 0);
+                moveDir *= (speed + 2);
+                moveDir = transform.TransformDirection(moveDir);
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                moveDir = new Vector3(0, 0, 0);
+
+            }
+        }
+
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+       
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 newPos = new Vector3();
+   
+    void FixedUpdate()
+    {
+        newPos.x = player.transform.position.x;
+        newPos.y = ((player.transform.position.y) + 2f);
+        newPos.z = ((player.transform.position.z) + 15f);
+
+        transform.position = newPos;
+
+    }
+}
